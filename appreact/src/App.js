2ff075d6703ed56bbc652bc1a6e89852b5e7616e
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-08-01 10:50:36 -0300

    Exercício JSX 2

--- appreact/src/App.js
+++ appreact/src/App.js
@@ -1,36 +1,49 @@
-const titulo = <h1>Esse é um título</h1>;
+// Mostre os dados da aplicação, como aprensetado no vídeo
+// Não utilize CSS externo, use o style para mudar as cores
+// Se a situação estiver ativa pinte de verde, inativa vermelho
+// Se o gasto for maior que 10000 mostre uma mensagem
+const luana = {
+    cliente: "Luana",
+    idade: 27,
+    compras: [
+        { nome: "Notebook", preco: "R$ 2500" },
+        { nome: "Geladeira", preco: "R$ 3000" },
+        { nome: "Smartphone", preco: "R$ 1500" },
+    ],
+    ativa: true,
+};
+
+const mario = {
+    cliente: "Mario",
+    idade: 31,
+    compras: [
+        { nome: "Notebook", preco: "R$ 2500" },
+        { nome: "Geladeira", preco: "R$ 3000" },
+        { nome: "Smartphone", preco: "R$ 1500" },
+        { nome: "Guitarra", preco: "R$ 3500" },
+    ],
+    ativa: false,
+};
 
 const App = () => {
-    const random = Math.random();
-    const ativo = false;
+    const dados = luana;
 
-    function mostrarNome(sobrenome) {
-        return "Gabriel " + sobrenome;
-    }
-
-    const carro = {
-        marca: "Ford",
-        rodas: "4",
-    };
-
-    const estiloP = {
-        color: "blue",
-        fontSize: "2rem",
-    };
+    const total = dados.compras
+        .map((compra) => Number(compra.preco.replace("R$ ", "")))
+        .reduce((total, preco) => total + preco);
 
     return (
         <>
-            {titulo}
+            <p>Nome: {dados.cliente}</p>
+            <p>Idade: {dados.idade}</p>
             <p>
-                {true ? "adasdsa" : "dsadaas"} - {10}
-                {mostrarNome("Tonhatti")}
+                Situação:{" "}
+                <span style={{ color: dados.ativa ? "green" : "red" }}>
+                    {dados.ativa ? "Ativa" : "Inativa"}
+                </span>
             </p>
-            <p style={estiloP}>{new Date().getFullYear()}</p>
-            <p style={estiloP}>{carro.marca}</p>
-            <p>{carro.rodas}</p>
-            <p className={ativo ? "ativo" : "inativo"}>
-                {(random * 1000) / 50}
-            </p>
+            <p>Total gasto: R$ {total}</p>
+            {total > 10000 && <p>Você está gastando muito.</p>}
         </>
     );
 };
