edba87e82031ff5d0c5150121f6212e978f8f797
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-08-01 18:59:44 -0300

    Propriedades

--- appreact/src/App.js
+++ appreact/src/App.js
@@ -1,26 +1,11 @@
 import React from "react";
-import Footer from "./Footer";
 import Form from "./Form/Form";
-import Header from "./Header";
-
-const Teste = () => {
-    const active = false;
-
-    if (active) {
-        return <p>Teste</p>;
-    }
-
-    return null;
-};
 
 const App = () => {
     return (
-        <section>
-            <Teste />
-            <Header />
+        <div>
             <Form />
-            <Footer />
-        </section>
+        </div>
     );
 };
 
--- appreact/src/Form/Form.js
+++ appreact/src/Form/Form.js
@@ -3,18 +3,14 @@ import Button from "./Button";
 import Input from "./Input";
 
 const Form = () => {
+    const array = ["Item 1", "Item 2"];
+
     return (
-        <form>
-            <p>
-                <label htmlFor="nome">Nome</label>
-                <Input />
-            </p>
-            <p>
-                <label htmlFor="email">Email</label>
-                <Input />
-            </p>
-            <Button />
-        </form>
+        <div>
+            <Input id="email" label="Email" required />
+            <Input id="password" type="password" label="Password" />
+            <Button itens={array} />
+        </div>
     );
 };
 
