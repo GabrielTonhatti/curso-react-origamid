74e825e4fc98834fc874e8815aa9b7775b80c35a
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-08-01 21:37:58 -0300

    Desafio useState

--- 03-react-hooks/0301-usestate/src/App.js
+++ 03-react-hooks/0301-usestate/src/App.js
@@ -1,20 +1,44 @@
-import React, { useState } from "react";
+import React from "react";
+import Produto from "./Produto";
+
+// Os links abaixo puxam dados de um produto em formato JSON
+// https://ranekapi.origamid.dev/json/api/produto/tablet
+// https://ranekapi.origamid.dev/json/api/produto/smartphone
+// https://ranekapi.origamid.dev/json/api/produto/notebook
+// Crie uma interface com 3 botões, um para cada produto.
+// Ao clicar no botão faça um fetch a api e mostre os dados do produto na tela.
+// Mostre apenas um produto por vez
+// Mostre a mensagem carregando... enquanto o fetch é realizado
 
 const App = () => {
-    const [contar, setContar] = useState(1);
-    const [items, setItems] = useState(["Item 1"]);
+    const [dados, setDados] = React.useState(null);
+    const [carregando, setCarregando] = React.useState(null);
 
-    function handleClick() {
-        setContar(contar + 1);
-        setItems([...items, `Item ${contar + 1}`]);
+    async function handleClick(event) {
+        setCarregando(true);
+        const response = await fetch(
+            `https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`
+        );
+
+        const json = await response.json();
+        console.log(json);
+        setDados(json);
+        setCarregando(false);
     }
 
     return (
         <div>
-            {items.map((item) => (
-                <li key={item}>{item}</li>
-            ))}
-            <button onClick={handleClick}>{contar}</button>
+            <button style={{ margin: ".5rem" }} onClick={handleClick}>
+                notebook
+            </button>
+            <button style={{ margin: ".5rem" }} onClick={handleClick}>
+                smartphone
+            </button>
+            <button style={{ margin: ".5rem" }} onClick={handleClick}>
+                tablet
+            </button>
+            {carregando && <p>Carregando...</p>}
+            {!carregando && dados && <Produto dados={dados} />}
         </div>
     );
 };
