969e551cf68ca9c1e7edb0754266ee19c6fb1101
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-08-02 23:28:52 -0300

    Exercício formulário - Input - Resposta

--- 04-formularios/0401-input/src/App.js
+++ 04-formularios/0401-input/src/App.js
@@ -1,8 +1,100 @@
 import React from "react";
-import Form from "./Form";
 
-function App() {
-    return <Form />;
-}
+const formFields = [
+    {
+        id: "nome",
+        label: "Nome",
+        type: "text",
+    },
+    {
+        id: "email",
+        label: "Email",
+        type: "email",
+    },
+    {
+        id: "senha",
+        label: "Senha",
+        type: "password",
+    },
+    {
+        id: "cep",
+        label: "Cep",
+        type: "text",
+    },
+    {
+        id: "rua",
+        label: "Rua",
+        type: "text",
+    },
+    {
+        id: "numero",
+        label: "Número",
+        type: "text",
+    },
+    {
+        id: "bairro",
+        label: "Bairro",
+        type: "text",
+    },
+    {
+        id: "cidade",
+        label: "Cidade",
+        type: "text",
+    },
+    {
+        id: "estado",
+        label: "Estado",
+        type: "text",
+    },
+];
+
+const forms = formFields.reduce((acc, field) => {
+    return {
+        ...acc,
+        [field.id]: "",
+    };
+}, {});
+
+const App = () => {
+    const [form, setForm] = React.useState(forms);
+    const [response, setResponse] = React.useState(null);
+
+    function handleChange({ target }) {
+        const { id, value } = target;
+        setForm({ ...form, [id]: value });
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        fetch("https://ranekapi.origamid.dev/json/api/usuario", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(form),
+        }).then((resp) => setResponse(resp));
+
+        setForm(forms);
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            {formFields.map(({ id, label, type }) => (
+                <div key={id}>
+                    <label htmlFor={id}>{label}</label>
+                    <input
+                        id={id}
+                        type={type}
+                        value={form[id]}
+                        onChange={handleChange}
+                    />
+                </div>
+            ))}
+            {response && response.ok && <p>Formulário Enviado</p>}
+            <button>Enviar</button>
+        </form>
+    );
+};
 
 export default App;
