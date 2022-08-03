a247c606540492ca535073637b5ff2b32da2fcd1
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-08-02 21:41:01 -0300

    Input 1

--- 04-formularios/0401-input/src/App.js
+++ 04-formularios/0401-input/src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
 
 function App() {
-    const [nome, setNome] = React.useState("");
-    const [email, setEmail] = React.useState("");
+    const [form, setForm] = React.useState({
+        nome: "",
+        email: "",
+    });
 
     function handleSubmit(event) {
         event.preventDefault();
         console.log(event);
+    }
+
+    function handleChange({ target }) {
+        const { id, value } = target;
+        console.log(id, value);
+
+        setForm({ ...form, [id]: value });
     }
 
     return (
@@ -15,15 +24,15 @@ function App() {
             <input
                 id="nome"
                 type="text"
-                value={nome}
-                onChange={(event) => setNome(event.target.value)}
+                value={form.nome}
+                onChange={handleChange}
             />
             <label htmlFor="email">Email</label>
             <input
                 id="email"
                 type="email"
-                value={email}
-                onChange={(event) => setEmail(event.target.value)}
+                value={form.email}
+                onChange={handleChange}
             />
             <button>Enviar</button>
         </form>
