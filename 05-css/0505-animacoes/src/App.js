8a0f00eb19ac989c68ecc181aab7390439e89d41
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-08-04 21:11:21 -0300

    Animações 1

---
+++ 05-css/0505-animacoes/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import React from "react";
+import Produto from "./Produto";
+
+const App = () => {
+    const [ativar, setAtivar] = React.useState(false);
+
+    return (
+        <div>
+            <button onClick={() => setAtivar(!ativar)}>Ativar</button>
+            {ativar && <Produto />}
+        </div>
+    );
+};
+
+export default App;
