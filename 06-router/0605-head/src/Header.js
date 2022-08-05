90e5e2951e8ad3f4ff5286c42282543775c56a0e
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-08-05 19:22:23 -0300

    Head

---
+++ 06-router/0605-head/src/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { NavLink, useLocation } from "react-router-dom";
+// import "./Header.css";
+
+const Header = () => {
+    const location = useLocation();
+
+    React.useEffect(() => {
+        console.log("Mudou a rota");
+    }, [location]);
+
+    return (
+        <nav>
+            <NavLink to="/" activeStyle={{ color: "tomato" }} end>
+                Home
+            </NavLink>
+            |{" "}
+            <NavLink to="sobre" activeStyle={{ color: "tomato" }}>
+                Sobre
+            </NavLink>
+        </nav>
+    );
+};
+
+export default Header;
---
+++ 06-router/0605-head/src/Sobre.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Head from "./Head";
+
+const Sobre = () => {
+    return (
+        <div>
+            <div>
+                <Head title="Sobre" description="Essa é a descrição da Sobre" />
+                <h1>Sobre</h1>
+                <p>Essa é a Sobre.</p>
+            </div>
+        </div>
+    );
+};
+
+export default Sobre;
