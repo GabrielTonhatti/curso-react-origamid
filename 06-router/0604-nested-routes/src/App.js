0ac463828280259c4969d212f4f1bf5086f1ec3c
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-08-05 19:06:50 -0300

    Nested Routes

---
+++ 06-router/0604-nested-routes/src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./Header";
+import Home from "./Home";
+import Login from "./Login";
+import NaoEncontrada from "./NaoEncontrada";
+import Produto from "./Produto";
+import ProdutoAvaliacao from "./ProdutoAvaliacao";
+import ProdutoCustomizado from "./ProdutoCustomizado";
+import ProdutoDescricao from "./ProdutoDescricao";
+import Sobre from "./Sobre";
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="sobre" element={<Sobre />} />
+                <Route path="login" element={<Login />} />
+                <Route path="produto/:id" element={<Produto />}>
+                    <Route path="/" element={<ProdutoDescricao />} />
+                    <Route path="/avaliacao" element={<ProdutoAvaliacao />} />
+                    <Route
+                        path="/customizado"
+                        element={<ProdutoCustomizado />}
+                    />
+                </Route>
+                <Route path="*" element={<NaoEncontrada />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
+
+export default App;
---
+++ 06-router/0604-nested-routes/src/Produto.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { NavLink, Outlet, useLocation, useParams } from "react-router-dom";
+
+const Produto = () => {
+    const params = useParams();
+    const location = useLocation();
+    console.log(location);
+
+    const search = new URLSearchParams(location.search);
+    console.log(search.get("memoria"));
+
+    return (
+        <div>
+            <h1>Produto: {params.id}</h1>
+            <nav>
+                <NavLink to="">Descrição</NavLink>
+                <NavLink to="avaliacao">Avaliação</NavLink>
+                <NavLink to="customizado">Customizado</NavLink>
+            </nav>
+            <Outlet />
+        </div>
+    );
+};
+
+export default Produto;
