f9e0ce8a4d92d42d83376f891cda390cc26becdf
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-08-04 22:05:18 -0300

    useNavigate

--- 06-router/0602-link/src/Header.js
+++ 06-router/0602-link/src/Header.js
@@ -7,10 +7,14 @@ const Header = () => {
         <nav>
             <NavLink to="/" activeStyle={{ color: "tomato" }} end>
                 Home
-            </NavLink>{" "}
+            </NavLink>
             |{" "}
             <NavLink to="sobre" activeStyle={{ color: "tomato" }}>
                 Sobre
+            </NavLink>
+            |{" "}
+            <NavLink to="login" activeStyle={{ color: "tomato" }}>
+                Login
             </NavLink>
         </nav>
     );
