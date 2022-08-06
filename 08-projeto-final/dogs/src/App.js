4baafd1f06ba195f00243d239f9a42500e00c618
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-08-06 13:42:15 -0300

    Estrutura inicial

--- 08-projeto-final/dogs/src/App.js
+++ 08-projeto-final/dogs/src/App.js
@@ -1,5 +1,24 @@
+import "./App.css";
+import React from "react";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
+
 function App() {
-    return <div>App React</div>;
+    return (
+        <div>
+            <BrowserRouter>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                </Routes>
+                <Footer />
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
