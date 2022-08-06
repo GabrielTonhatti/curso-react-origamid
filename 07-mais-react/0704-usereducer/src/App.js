c04d523455568ebaa497cde246d6905867282705
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-08-06 11:59:36 -0300

    UseReducer

---
+++ 07-mais-react/0704-usereducer/src/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Exemplo from "./Exemplo";
+
+const reducer = (state, action) => {
+    console.log(action);
+
+    switch (action) {
+        case "aumentar":
+            return state + 1;
+        case "diminuir":
+            return state - 1;
+        default:
+            throw new Error();
+    }
+
+    // if (action === "aumentar") {
+    //     return state + 1;
+    // }
+
+    // if (action === "diminuir") {
+    //     return state - 1;
+    // }
+
+    // return new Error("Error action não existe");
+};
+
+const App = () => {
+    const [state, dispatch] = React.useReducer(reducer, 0);
+
+    return (
+        <div>
+            <button onClick={() => dispatch("aumentar")}>+</button>
+            <button onClick={() => dispatch("diminuir")}>-</button>
+            <p>{state}</p>
+            <Exemplo />
+        </div>
+    );
+};
+
+export default App;
