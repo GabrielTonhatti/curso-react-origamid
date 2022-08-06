ec25f94650aa4e2edf24b5efa8e39c71710fdacf
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-08-06 10:50:48 -0300

    Desafio Router 3

--- 06-router/0606-desafio-router/src/Components/Produto.js
+++ 06-router/0606-desafio-router/src/Components/Produto.js
@@ -29,7 +29,7 @@ const Produto = () => {
         fetchProduto(`https://ranekapi.origamid.dev/json/api/produto/${id}`);
     }, [id]);
 
-    if (loading) return <div>Carregando...</div>;
+    if (loading) return <div className="loading"></div>;
     if (error) return <p>{error}</p>;
     if (produto === null) return null;
 
@@ -39,9 +39,11 @@ const Produto = () => {
                 title={`Ranek | ${produto.nome}`}
                 description={`Ranek | Esse é um produto: ${produto.nome}`}
             />
-            {produto.fotos.map((foto) => (
-                <img key={foto.src} src={foto.src} alt={foto.titulo} />
-            ))}
+            <div>
+                {produto.fotos.map((foto) => (
+                    <img key={foto.src} src={foto.src} alt={foto.titulo} />
+                ))}
+            </div>
             <div>
                 <h1>{produto.nome}</h1>
                 <span className={styles.preco}>R$ {produto.preco}</span>
