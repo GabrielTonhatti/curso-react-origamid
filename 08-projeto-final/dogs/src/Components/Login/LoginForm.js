295366c410bf9fc641e4d75709c3e97eb971f607
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-08-07 11:05:16 -0300

    Login form

--- 08-projeto-final/dogs/src/Components/Login/LoginForm.js
+++ 08-projeto-final/dogs/src/Components/Login/LoginForm.js
@@ -4,6 +4,9 @@ import useForm from "../../Hooks/useForm";
 import { UserContext } from "../../UserContext";
 import Button from "../Forms/Button";
 import Input from "../Forms/Input";
+import Error from "../Helper/Error";
+import styles from "./LoginForm.module.css";
+import stylesBtn from "../Forms/Button.module.css";
 
 const LoginForm = () => {
     const username = useForm();
@@ -19,9 +22,9 @@ const LoginForm = () => {
     }
 
     return (
-        <section>
-            <h1>Login</h1>
-            <form onSubmit={handleSubmit}>
+        <section className="animeLeft">
+            <h1 className="title">Login</h1>
+            <form className={styles.form} onSubmit={handleSubmit}>
                 <Input
                     label="Usuário"
                     type="text"
@@ -36,15 +39,23 @@ const LoginForm = () => {
                 />
                 {loading ? (
                     <Button disabled>
-                        <div className="loading-button"></div>
+                        <div className={styles.loading}></div>
                     </Button>
                 ) : (
                     <Button>Entrar</Button>
                 )}
-                {error && <p>{error}</p>}
+                <Error error={error} />
             </form>
-
-            <Link to="/login/criar">Cadastro</Link>
+            <Link className={styles.perdeu} to="/login/perdeu">
+                Perdeu a Senha?
+            </Link>
+            <div className={styles.cadastro}>
+                <h2 className={styles.subtitle}>Cadastre-se</h2>
+                <p>Ainda não possui conta? Cadastre-se no site.</p>
+                <Link className={stylesBtn.button} to="/login/criar">
+                    Cadastro
+                </Link>
+            </div>
         </section>
     );
 };
