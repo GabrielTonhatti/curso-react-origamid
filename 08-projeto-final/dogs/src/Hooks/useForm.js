6a3e26098ad055fc07b54a3e55e552e91f6694e1
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-08-07 15:14:04 -0300

    login Criar

--- 08-projeto-final/dogs/src/Hooks/useForm.js
+++ 08-projeto-final/dogs/src/Hooks/useForm.js
@@ -5,6 +5,11 @@ const types = {
         regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
         message: "Preencha um email válido",
     },
+    password: {
+        regex: /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([a-zA-Z0-9]{8})$/,
+        message:
+            "A senha precisa ter 1 caracter maísculo, 1 minúsculo e 1 digito. Com no mínimo 8 caracteres.",
+    },
 };
 
 const useForm = (type) => {
